c60f2daaee718934f64f54a52c7f49497cf62f01
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-31 09:29:48 +0000

    A button to the admin page

--- vault/settings.py
+++ vault/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('vault_secret_key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = DEBUG = str(os.environ.get('DEBUG')) == "1"
+DEBUG = str(os.environ.get('DEBUG')) == "1"
 
 ALLOWED_HOSTS = ['127.0.0.1', 'yonnaforexvault.pythonanywhere.com']
 
