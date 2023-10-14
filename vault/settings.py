dd53ba538698a76fea22f56821b363bdf0d9ef9f
Lamin Jawla <laminjawla1001@gmail.com>
2023-10-14 15:22:25 +0000

    Ready for deployment

--- vault/settings.py
+++ vault/settings.py
@@ -95,9 +95,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('yonna_vault_db_password'),
         'HOST': os.environ.get('database_host'),
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET default_storage_engine=INNODB',
-        }
+       'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
