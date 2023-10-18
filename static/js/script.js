701a97ad176e14b5fd6dbdcbdd93af3fc8110608
Lamin Jawla <laminjawla1001@gmail.com>
2023-10-18 20:42:35 +0000

    Show date and time in realtime

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,22 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+function toggleItems(categoryId) {
+  const items = document.getElementById(categoryId + 'Items');
+  items.classList.toggle('hidden');
+}
+function updateLiveDate() {
+  let currentDate = new Date();
+  let options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day:'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      timeZoneName: 'short',
+      hour12: false
+  };
+  let formattedDate = currentDate.toLocaleDateString(undefined, options);
+  document.getElementById('current-date').innerHTML = formattedDate;
+}
+setInterval(updateLiveDate, 1000);
+updateLiveDate();
